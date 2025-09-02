cf504dbe67ac81229c39339ccb4d76029e8cd46c
Sitong <nickren93@gmail.com>
2025-09-02 00:39:44 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -53,5 +53,40 @@ api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
 
+# below are new code written by me:
+
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+
+        session['user_id'] = user.id
+
+        response = make_response(jsonify(user.to_dict()), 200)
+        return response
+    
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+    
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            response = make_response(jsonify(user.to_dict()), 200)
+            return response
+        else:
+            # return {}, 401
+            response = make_response(jsonify({}), 401)
+            return response
+
+
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
